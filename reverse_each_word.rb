24053eadf16259f9df3bdf5662b249d692d70948
Vishal Sharma <vsh1819@gmail.com>
2018-06-08 16:28:11 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,10 @@ def reverse_each_word(string)
   array = string.split(" ")
   new_array = []
   array.each do |word|
-    
+    chars = word.split("")
+    new_word = []
+    chars.each do |char|
+      
+    end
   end
 end
