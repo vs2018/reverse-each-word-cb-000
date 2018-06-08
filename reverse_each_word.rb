d64ad62afa2eb74377fb57cf3f01acc498f2955a
Vishal Sharma <vsh1819@gmail.com>
2018-06-08 16:34:09 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -14,5 +14,16 @@
 # end
 
 def reverse_each_word(string)
-  
+    array = string.split(" ")
+  new_array = []
+  array.each do |word|
+    chars = word.split("")
+    new_word = []
+    chars.each do |char|
+      new_word.unshift(char)
+    end
+    string = new_word.join
+    array.last != word ? new_array << string + " " : new_array << string
+  end
+  return new_array.join
 end
