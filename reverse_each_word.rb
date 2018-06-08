d196c627dc9b47905c1ff1cc93e2abc44a14844e
Vishal Sharma <vsh1819@gmail.com>
2018-06-08 16:36:55 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -16,6 +16,17 @@
 def reverse_each_word(string)
   array = string.split(" ")
   array.collect do |word|
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     chars = word.split("")
     new_word = []
     chars.each do |char|
