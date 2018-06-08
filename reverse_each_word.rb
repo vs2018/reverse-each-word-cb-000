772d9f93f255366346980e18575939cc8f6344c1
Vishal Sharma <vsh1819@gmail.com>
2018-06-08 16:32:47 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,8 +8,7 @@ def reverse_each_word(string)
       new_word.unshift(char)
     end
     string = new_word.join
-    complete_string = string + " "
-    new_array << string
+    array.last != word ? new_array << string + " " : new_array << string
   end
   return new_array.join
 end
